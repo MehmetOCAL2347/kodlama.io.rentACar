cb21e01ea4c643ec787acb0a43860327b330f16c
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-02-20 08:59:08 +0300

    BrandController'a yeni oluşturulan requestler eklendi

--- src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -2,9 +2,10 @@ package kodlama.io.rentACar.webApi.controllers;
 
 import kodlama.io.rentACar.business.abstracts.BrandServise;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +23,24 @@ public class BrandsController { // Api isimleri çoğul verilir
         return brandServise.getAll();
     }
 
+    @GetMapping("/{id}")
+    public GetByIdBrandResponse getByIdBrandResponse(@PathVariable int id){
+        return brandServise.getById(id);
+    }
+
     @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
     public void add(@RequestBody CreateBrandRequest createBrandRequest){
         this.brandServise.add(createBrandRequest);
+    }
+
+    @PutMapping()
+    public void update(@RequestBody UpdateBrandRequest updateBrandRequest){
+        this.brandServise.update(updateBrandRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        this.brandServise.delete(id);
     }
 }
