f9cf2dd9173d521976ffa1e422648f0821fbca48
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-02-20 08:27:30 +0300

    BrandsController'da @AllArgsConstructor annotation eklendi ve mappinglerden endpointsler kaldırıldı

--- src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -3,6 +3,7 @@ package kodlama.io.rentACar.webApi.controllers;
 import kodlama.io.rentACar.business.abstracts.BrandServise;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,21 +12,17 @@ import java.util.List;
 
 @RestController // Rest controller oldugunu belirledik
 @RequestMapping("/api/brands") // Adresleme -> request atacağımız adres
+@AllArgsConstructor
 public class BrandsController { // Api isimleri çoğul verilir
 
     private BrandServise brandServise;
 
-    @Autowired // Springin yeni versiyonlarında yazmasakta oluyor
-    public BrandsController(BrandServise brandServise) {
-        this.brandServise = brandServise;
-    }
-
-    @GetMapping("/getall")
+    @GetMapping()
     public List<GetAllBrandsResponse> getAll(){
         return brandServise.getAll();
     }
 
-    @PostMapping("/createNewCar")
+    @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
     public void add(@RequestBody CreateBrandRequest createBrandRequest){
         this.brandServise.add(createBrandRequest);
