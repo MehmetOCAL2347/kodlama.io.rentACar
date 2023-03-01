cca893e8cadc6b47014e07310edbb5c7b63df535
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-03-01 23:19:46 +0300

    Car objesi oluşturuldu ve GetAllModelsResponse response'u oluşturuldu

--- src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
@@ -5,28 +5,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-@Table(name = "models")
+@Table(name = "cars")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Model { // Araçlara ait modelleri kullanacağız
+public class Car {
 
     @Id // Bu değişken bir primary keydir anlamına gelir
     @GeneratedValue(strategy = GenerationType.IDENTITY) //bu değeri 1 er 1 er artır otomatik olarak anlamına gelir
     @Column(name = "id")
     private int id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "plate", unique = true)
+    private String plate; // Plaka
+
+    @Column(name = "dailyPrice")
+    private double dailyPrice;
+
+    @Column(name = "modelYear")
+    private int modelYear;
+
+    @Column(name = "state")
+    private int state; // 1.Available, 2.Rented, 3.Maintanance  --> Durumu
 
     @ManyToOne
-    @JoinColumn(name = "brand_id")
-    private Brand brand;
-
-    @OneToMany(mappedBy = "model")
-    private List<Car> cars;
+    @JoinColumn(name = "model_id")
+    private Model model;
 
 }
